056d9dcaa0684c75109a7c633ca9c4617792ec6d
kartik kumar <gtgkartik@gmail.com>
2024-01-14 08:56:18 +0530

    added

---
+++ src/com/firstjavaprogram/assignments/Problem3.java
@@ -0,0 +1,8 @@
+package com.firstjavaprogram.assignments;
+
+//Write a program to input principal, time, and rate (P, T, R) from the user and find Simple Interest.
+
+// formula
+public class Problem3 {
+
+}
