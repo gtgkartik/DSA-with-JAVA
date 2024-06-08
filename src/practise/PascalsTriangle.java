3716018d2bc9ca6cdbafb025e4f6a7548e3f4e66
kartik kumar <gtgkartik@gmail.com>
2024-06-08 14:47:22 +0530

    code for findNCR pascals triangle

--- src/practise/PascalsTriangle.java
+++ src/practise/PascalsTriangle.java
@@ -1,4 +1,17 @@
 package practise;
 
 public class PascalsTriangle {
+
+    static long findncr(int n , int r){
+        long res = 1 ;
+        for(int i = 0 ; i < r; i++){
+            res = (long) (n-i) *res ;
+            res =  res/(1+i) ;
+        }
+        return res ;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(findncr(5,2));
+    }
 }
