6fd69c7e4d659631627da89140d46a77dbcf943f
kartik kumar <gtgkartik@gmail.com>
2024-06-08 11:36:06 +0530

    added

--- src/com/Arrays_and_ArrayLists/Inputtoarray.java
+++ src/com/Arrays_and_ArrayLists/Inputtoarray.java
@@ -8,7 +8,7 @@ public class Inputtoarray {
         Scanner sc = new Scanner(System.in) ;
 
         int[] arr = new int[10];
-        //added
+        //addedaa
 
         for (int i = 0; i < arr.length; i++) {
             arr[i] = sc.nextInt() ;
