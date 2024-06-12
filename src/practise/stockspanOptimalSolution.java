e4355aeb67f14155d3a07c405ed6137f4fd0cd49
kartik kumar <gtgkartik@gmail.com>
2024-06-12 13:59:17 +0530

    stock span optimal solution

---
+++ src/practise/stockspanOptimalSolution.java
@@ -0,0 +1,23 @@
+package practise;
+
+public class stockspanOptimalSolution {
+    public static void main(String[] args) {
+        int arr[] = {7, 1, 5, 3, 6, 4};
+
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for(int i = 0; i < arr.length; i++) {
+            // Update the minimum price encountered so far
+            if(arr[i] < minPrice) {
+                minPrice = arr[i];
+            }
+            // Calculate the potential profit at the current price and update maxProfit if it's higher
+            else if(arr[i] - minPrice > maxProfit) {
+                maxProfit = arr[i] - minPrice;
+            }
+        }
+
+        System.out.println(maxProfit);
+    }
+}
