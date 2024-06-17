c8caf4b3e45ba69dff31aa8e52acbcba41f7e4c1
kartik kumar <gtgkartik@gmail.com>
2024-06-17 22:52:05 +0530

    reverse a linked list

--- src/scott/LinkedListPractise/LinkedListImplementation2.java
+++ src/scott/LinkedListPractise/LinkedListImplementation2.java
@@ -18,7 +18,7 @@ class LinkedList {
     public void printAll(){
         Node temp = head ;
         while(temp !=null){
-            System.out.println(temp.value);
+            System.out.print(temp.value+ " ");
             temp = temp.next ;
         }
     }
@@ -45,6 +45,67 @@ class LinkedList {
         }
     }
 
+    public void getIndex(int index){
+        Node temp = head ;
+        for(int i = 0 ; i < index; i ++){
+            temp = temp.next ;
+        }
+        System.out.println("value at index: " + temp.value);
+    }
+
+    public void setIndex(int index, int value){
+        Node temp = head ;
+
+        for(int i = 0; i<index; i++){
+            temp = temp.next ;
+        }
+        System.out.println("value: " + temp.value);
+        temp.value = value ;
+        System.out.println("updated value: " + temp.value);
+    }
+
+    public void insert(int index, int value){
+        Node temp = head ;
+        for(int i = 0 ; i < index ; i ++){
+            temp = temp.next ;
+        }
+        Node newNode = new Node(value);
+        Node next = temp.next ;
+        temp.next = newNode;
+        newNode.next = next ;
+    }
+
+    public void remove(int index){
+        Node temp = head ;
+        for( int i=0 ; i<index-1; i ++){
+            temp = temp.next;
+        }
+
+        Node next = temp.next.next ;
+        temp.next = next ;
+
+    }
+
+    public void reverse() {
+        Node prev =null;
+        Node current = head;
+        while (current != null) {
+            Node next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        head = prev ;
+        tail = head ;
+    }
+
+
+    public void getHead(){
+        System.out.println("head:" + head.value);
+    }
+    public void getTail(){
+        System.out.println("tail: "+ tail.value);
+    }
     public LinkedList(int value){
         Node newNode = new Node(value);
         head = newNode ;
@@ -56,6 +117,17 @@ public class LinkedListImplementation2 {
         LinkedList list = new LinkedList(2);
         list.append(5);
         list.prepend(100);
+        list.setIndex(0,200);
+        list.insert(1,1000);
+        list.remove(1);
+        list.append(9);
+        list.append(10);
         list.printAll();
+        list.getIndex(2);
+        list.getHead();
+        list.getTail();
+        list.reverse();
+        list.printAll();
+        list.getHead();
     }
 }
