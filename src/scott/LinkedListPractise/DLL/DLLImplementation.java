9654778f88f45565765657ab550433a74df24f4b
kartik kumar <gtgkartik@gmail.com>
2024-06-20 17:34:13 +0530

    DLL append prepend, remove, get , removefirst, removened

--- src/scott/LinkedListPractise/DLL/DLLImplementation.java
+++ src/scott/LinkedListPractise/DLL/DLLImplementation.java
@@ -19,18 +19,81 @@ class DLL{
     }
 
     void getHead(){
-        System.out.println(head.value);
+        System.out.println("head: "+head.value);
     }
 
     void getTail (){
-        System.out.println(tail.value);
+        System.out.println("tail: "+tail.value);
     }
 
     void printAll(){
         Node temp = head ;
         while(temp!=null){
-            System.out.println(temp.value);
+            System.out.print(temp.value+" ");
+            temp = temp.next ;
         }
+    }
+
+    void append(int value){
+        Node newNode = new Node(value);
+        if(head== null){
+            head = newNode ;
+            tail= newNode;
+        }else{
+            newNode.prev = tail ;
+            tail.next = newNode ;
+            tail = newNode ;
+
+        }
+    }
+
+    void removeLast(){
+        if(head == tail){
+            head = null; tail = null ;
+        }else{
+            Node prevNode = tail.prev ;
+            prevNode.next=null ;
+            tail.prev = null;
+            tail = prevNode ;
+        }
+    }
+
+    void prepend(int value){
+        Node newNode = new Node(value);
+        head.prev = newNode ;
+        newNode.next = head ;
+        head = newNode ;
+    }
+
+    void removeFirst(){
+        Node nextNode = head.next ;
+        nextNode.prev = null ;
+        head.next = null ;
+        head = nextNode ;
+    }
+
+    void insert(int index, int value){
+        Node temp = head ;
+        for(int i = 0 ; i < index; i ++){
+            temp = temp.next ;
+        }
+        Node newNode = new Node(value);
+        Node prevNode = temp.prev ;
+
+        prevNode.next = newNode ;
+        newNode.prev = prevNode ;
+        newNode.next = temp ;
+        temp.prev = newNode ;
+
+    }
+
+    void remove(int index){
+        Node temp = head ;
+        for(int i = 0 ; i < index ; i ++){
+            temp = temp.next;
+        }
+        temp.prev.next = temp.next ;
+        temp.next.prev = temp.prev ;
     }
 }
 
@@ -39,6 +102,15 @@ public class DLLImplementation {
 
     public static void main(String[] args) {
         DLL dl = new DLL(2);
+        dl.prepend(100);
+        dl.removeFirst();
+        dl.append(3);
+        dl.insert(1,143);
+        dl.printAll();
         dl.getHead();
+        dl.getTail();
+        dl.remove(1);
+        dl.printAll();
+
     }
 }
