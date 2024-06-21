3b8a99a560df0651adcf759fa3674f61069a11f4
kartik kumar <gtgkartik@gmail.com>
2024-06-21 12:55:56 +0530

    prefix sum of an array

---
+++ src/practise/PrefixSum.java
@@ -0,0 +1,23 @@
+package practise;
+
+public class PrefixSum {
+
+    static int[] prefixSum(int[] arr){
+        int[] ps = new int[arr.length];
+
+        ps[0] = 1 ;
+        for(int i =1;  i<arr.length ; i++){
+            ps[i] = ps[i-1]+arr[i-1];
+        }
+        return ps ;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,5,6};
+
+        int [] res = prefixSum(arr);
+        for(int a : res){
+            System.out.println(a);
+        }
+    }
+}
