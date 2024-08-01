0bc91a18d880084c77b1b01f8f5095b3db479dc9
kartik kumar <gtgkartik@gmail.com>
2024-08-01 18:00:35 +0530

    add find mountain array

--- src/LeetCodePractise/FindMountainArray/FIndMountainArray.java
+++ src/LeetCodePractise/FindMountainArray/FIndMountainArray.java
@@ -2,9 +2,7 @@ package LeetCodePractise.FindMountainArray;
 
 public class FIndMountainArray {
 
-    static int searchAscending(int [] arr , int target){
-        int end = findPeak(arr);
-        int start = 0 ;
+    static int searchAscending(int start, int end , int [] arr , int target){
 
         while(start<=end){
             int mid = start + (end-start)/2 ;
@@ -20,7 +18,7 @@ public class FIndMountainArray {
     }
 
     static int searchDescending(int [] arr , int target){
-        int start = findPeak(arr);
+        int start = findPivot(arr);
         int end  = arr.length-1 ;
 
         while(start<=end){
@@ -37,31 +35,56 @@ public class FIndMountainArray {
         return -1 ;
     }
 
-    static int findPeak(int arr[] ){
-        int start =  0 ;
-        int end = arr.length-1 ;
+//    static int findPeak(int arr[] ){
+//        int start =  0 ;
+//        int end = arr.length-1 ;
+//
+//        while(start<end){
+//            int mid = start + (end-start)/2 ;
+//            if(arr[mid]>arr[mid+1]){
+//                end = mid;
+//            }else{
+//                start = mid+1;
+//            }
+//        }
+//        return start ;
+//    }
 
-        while(start<end){
-            int mid = start + (end-start)/2 ;
-            if(arr[mid]>arr[mid+1]){
-                end = mid;
+    static int findPivot(int arr []){
+        int start = 0 ;
+        int end = arr.length -1  ;
+
+        while(start<=end){
+            int mid = start+ (end - start)/2 ;
+            if(mid<end && arr[mid] >  arr[mid+1]){
+                return mid ;
+            }
+
+            if( mid > start && arr[mid]<arr[mid-1]){
+                return mid ;
+            }
+
+            if(arr[mid]<=arr[start]){
+                end = mid-1 ;
             }else{
-                start = mid+1;
+                start = mid +1 ;
             }
         }
-        return start ;
+
+        return -1 ;
     }
 
 
     public static void main(String[] args) {
-        int arr[] = {1,2,2,4,5,3,1};
-        int target = 3 ;
-       int left = searchAscending(arr,target);
-       if(left!=-1){
-           System.out.println(left);
-       }else{
-           System.out.println( searchDescending(arr,target));
-       }
+        int arr[] = {5,1,3};
+        int target = 1 ;
+
+        int pivot = findPivot(arr);
+        System.out.println(pivot);
+        int left = searchAscending(0 , pivot ,arr, target);
+        if(left == -1){
+            System.out.println(searchAscending(pivot+1, arr.length-1, arr, target));
+        }
 
     }
 }
